e3c0e184074f13085a147049652f471520185048
znyeokay <nanye.zou@gmail.com>
2023-03-26 22:58:42 +1030

    Create ft_memchr.c

---
+++ ft_memchr.c
@@ -0,0 +1,12 @@
+void *memchr(const void *s, int c, size_t n);
+
+/*
+The memchr() function locates the first occurrence of c (converted to an
+unsigned char) in string s.
+The memchr() function returns a pointer to the byte located, or NULL if
+no such byte exists within n bytes.
+*/
+/*
+NOTE
+
+*/
