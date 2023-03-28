fd5729518dffcfa928df72b68f700db5b129ce04
Nanye Zou <55478832+znyeoaky@users.noreply.github.com>
2023-03-28 16:53:25 +1030

    Update ft_strchr.c

--- libft_draft/ft_strchr.c
+++ libft_draft/ft_strchr.c
@@ -1,3 +1,22 @@
 #include "libft.h"
 
 char *strchr(const char *s, int c);
+
+
+
+
+/*
+DESCRIPTION
+	The strchr() function locates the first occurrence of c (converted to a
+     char) in the string pointed to by s.  The terminating null character is
+     considered to be part of the string; therefore if c is `\0', the func-
+     tions locate the terminating `\0'.
+
+     The functions strchr() and strrchr() return a pointer to the located
+     character, or NULL if the character does not appear in the string.
+*/
+
+/*
+NOTE
+
+*/
