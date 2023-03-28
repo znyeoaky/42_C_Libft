993ffbb5df0977f40789ed01a830a252443215d6
Nanye Zou <55478832+znyeoaky@users.noreply.github.com>
2023-03-28 16:53:27 +1030

    Update ft_strrchr.c

--- libft_draft/ft_strrchr.c
+++ libft_draft/ft_strrchr.c
@@ -1,3 +1,20 @@
 #include "libft.h"
 
 char *strrchr(const char *s, int c);
+
+
+
+
+/*
+DESCRIPTION
+	The strrchr() function is identical to strchr(), except it locates the
+     last occurrence of c.
+
+     The functions strchr() and strrchr() return a pointer to the located
+     character, or NULL if the character does not appear in the string.
+*/
+
+/*
+NOTE
+    
+*/
