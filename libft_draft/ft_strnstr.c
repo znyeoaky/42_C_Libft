308cbcc2915e828ac4b1829646076e8a6c5d13b2
Nanye Zou <55478832+znyeoaky@users.noreply.github.com>
2023-03-29 16:03:30 +1030

    Update ft_strnstr.c

--- libft_draft/ft_strnstr.c
+++ libft_draft/ft_strnstr.c
@@ -1,6 +1,42 @@
 #include "libft.h"
 
-char *	strnstr(const char *haystack, const char *needle, size_t len);
+char *	ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t i;
+	size_t j;
+
+	if(!*needle)
+		return ((char*)haystack);
+	
+	i = 0;
+	while(haystack[i]){
+		j = 0;
+		while(needle[j] == haystack[i + j] && haystack[i + j] != '\0'){
+			j++;
+		}
+		if(needle[j] == '\0')
+			return ((char *)(haystack + i));
+		i++;
+	}
+
+	return (NULL);
+}
+
+// int main() {
+// 	const char *haystack = "lask askdfal eak asd asdfeae aoo";
+// 	const char *needle = "aoo";
+
+// 	const char *haystack1 = "lask askdfal eak asd asdfeae aoo";
+// 	const char *needle1 = "aoo";
+
+// 	char *result = strnstr(haystack, needle, strlen(haystack));
+// 	printf("strnstr:	'%s' at %ld\n", result, result - haystack);
+
+// 	char *result1 = ft_strnstr(haystack1, needle1, strlen(haystack1));
+// 	printf("ft_strnstr:	'%s' at %ld\n", result1, result1 - haystack1);
+
+// 	return 0;
+// }
 
 
 /*
@@ -15,8 +51,46 @@ DESCRIPTION
 	should only be used when portability is not a concern.
 
 RETURN VALUES
-	If needle is an empty string, haystack is returned; if needle occurs nowhere in haystack, NULL is
-	returned; otherwise a pointer to the first character of the first occurrence of needle is
-	returned.
+	If needle is an empty string, haystack is returned; 
+	if needle occurs nowhere in haystack, NULL is returned; 
+	otherwise a pointer to the first character of the first occurrence of needle is	returned.
 */
+
+/*
+NOTE
+WHY"are they same? 
+		#while(needle[j] == haystack[i + j] && haystack[i + j] != '\0') 
+		and #while(needle[j] == haystack[i + j] && needle[j] != '\0')
+	"
+	TEST:
+
+
+*/
+
+
+int main() {
+    const char *needle = "ab";
+    const char *haystack = "abababab";
+    int i = 0, j = 0;
+
+    // First condition: checks both the end of the haystack and the needle
+    while(needle[j] == haystack[i + j] && haystack[i + j] != '\0') {
+        j++;
+        if(needle[j] == '\0') {
+            printf("1Found the substring at index %d\n", i);
+            break;
+        }
+    }
+
+    // Second condition: checks only the end of the needle
+    while(needle[j] == haystack[i + j] && needle[j] != '\0') {
+        j++;
+        if(needle[j] == '\0') {
+            printf("2Found the substring at index %d\n", i);
+            break;
+        }
+    }
+
+    return 0;
+}
 
