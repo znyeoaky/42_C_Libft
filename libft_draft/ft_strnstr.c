d3576113124f7a57725d4775be28e2749c68e66a
Nanye Zou <55478832+znyeoaky@users.noreply.github.com>
2023-03-29 13:57:50 +1030

    Create ft_strnstr.c

---
+++ libft_draft/ft_strnstr.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+char *	strnstr(const char *haystack, const char *needle, size_t len);
+
+
+/*
+	#include <string.h>
+DESCRIPTION
+	The strstr() function locates the first occurrence of the null-terminated string needle in the
+	null-terminated string haystack.
+
+	The strnstr() function locates the first occurrence of the null-terminated string needle in the
+	string haystack, where not more than len characters are searched.  Characters that appear after a
+	`\0' character are not searched.  Since the strnstr() function is a FreeBSD specific API, it
+	should only be used when portability is not a concern.
+
+RETURN VALUES
+	If needle is an empty string, haystack is returned; if needle occurs nowhere in haystack, NULL is
+	returned; otherwise a pointer to the first character of the first occurrence of needle is
+	returned.
+*/
+
